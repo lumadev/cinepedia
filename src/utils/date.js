2eba2e6bc5a29dab5533d1e9d91cbe6775489cd8
Luma Macagnan <lumamacagnan@gmail.com>
2025-10-05 20:51:05 -0300

    fix format date function

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,11 +1,6 @@
 function formatDate(dateString) {
-  const date = new Date(dateString)
-
-  const day = String(date.getDate()).padStart(2, '0')
-  const month = String(date.getMonth() + 1).padStart(2, '0') // mês é zero-based
-  const year = date.getFullYear()
-
-  return `${day}/${month}/${year}`
+  const [year, month, day] = dateString.split("-");
+  return `${day}/${month}/${year}`;
 }
 
 export { formatDate }
