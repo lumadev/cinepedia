d8b3332c8255a9b47e1d05a7f9e70bfe1a2112df
Luma Macagnan <lumamacagnan@gmail.com>
2026-02-14 21:40:51 -0300

    change home architecture to controller
    improve hooks organization with folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,106 +1,51 @@
 "use client";
 
-import { useState, useEffect, useCallback, useDeferredValue } from "react";
 import { MovieList } from "@/components/movies/MovieList";
 import { AddMovieModal } from "@/components/movies/AddMovieModal";
-import { Movie } from "@/components/movies/interfaces/movie";
 import { Toast } from "@/components/ui/Toast";
 import { SkeletonLoader } from "@/components/ui/SkeletonLoader";
 import { MoviesNavbar } from "@/components/layout/MoviesNavbar";
 
-import { useMovies } from "@/hooks/useMovies";
-import { useFilteredMovies } from "@/hooks/useFilteredMovies";
-
-import { loadLastOrder } from "@/components/movies/helpers/loadLastOrder";
+import { useMoviesPageController } from "@/hooks/pages/useMoviesPageController";
 
 export default function Home() {
-  // loading movies hook
-  const { movies, loading: loadingMovies, error: errorLoadingMovies, fetchMovies } = useMovies();
-  
-  const [loadingLastOrder, setLoadingLastOrder] = useState(false);
-
-  const [lastOrder, setLastOrder] = useState(1);
-  const [isEdit, setIsEdit] = useState(false);
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
-  const [searchQuery, setSearchQuery] = useState("");
-  const filteredMovies = useFilteredMovies(movies, searchQuery);
-  
-  // modal state
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [movieToEdit, setMovieToEdit] = useState<Movie | null>(null);
-
-  const loadInitialData = useCallback(() => {
-    loadLastOrder(setLastOrder, setErrorMessage, setLoadingLastOrder);
-  }, []);
-
-  const onAfterDeleteAction = useCallback(() => {
-    fetchMovies()
-     // Atualiza a última ordem
-    loadInitialData();
-    setMovieToEdit(null);
-  }, [fetchMovies, loadInitialData]);
-
-  const onAfterSaveAction = useCallback(() => {
-    fetchMovies()
-    setIsEdit(false);
-    setMovieToEdit(null);
-  }, [])
-
-  const handleAddMovie = () => {
-    setIsEdit(false);
-    setMovieToEdit(null);
-    setIsModalOpen(true);
-  };
-
-  const handleEditMovie = (movie: Movie) => {
-    setIsEdit(true);
-    setMovieToEdit(movie);
-    setIsModalOpen(true);
-  };
-  
-  useEffect(() => {
-    loadInitialData()
-  }, [loadInitialData]);
-
-  const displayError = errorLoadingMovies || errorMessage;
-  const hasSearch = searchQuery.trim().length > 0;
+  const controller = useMoviesPageController();
 
   return (
     <div className="min-h-screen bg-[var(--background)] text-[var(--foreground)] transition-colors">
       <MoviesNavbar
-        onSearchChange={setSearchQuery}
-        onAddMovie={handleAddMovie}
+        onSearchChange={controller.setSearchQuery}
+        onAddMovie={controller.handleAddMovie}
       />
 
       <div className="p-6">
-        {loadingMovies || loadingLastOrder ? (
+        {controller.loading ? (
           <SkeletonLoader count={10} />
         ) : (
           <>
             <MovieList
-              movies={filteredMovies}
-              hasSearch={hasSearch}
-              onAfterDeleteAction={onAfterDeleteAction}
-              onEditMovieAction={handleEditMovie}
+              movies={controller.filteredMovies}
+              hasSearch={controller.hasSearch}
+              onAfterDeleteAction={controller.onAfterDelete}
+              onEditMovieAction={controller.handleEditMovie}
             />
 
             <AddMovieModal
-              isOpen={isModalOpen}
-              onClose={() => setIsModalOpen(false)}
-              onAfterSave={onAfterSaveAction}
-              isEdit={isEdit}
-              movie={movieToEdit}
-              lastOrder={lastOrder}
+              isOpen={controller.isModalOpen}
+              onClose={() => controller.closeModal}
+              onAfterSave={controller.onAfterSave}
+              isEdit={controller.isEdit}
+              movie={controller.movieToEdit}
+              lastOrder={controller.lastOrder}
             />
           </>
         )}
 
-        {displayError && (
+        {controller.displayError && (
           <Toast
-            message={displayError}
+            message={controller.displayError}
             type="error"
-            onClose={() => setErrorMessage(null)}
+            onClose={controller.closeModal}
           />
         )}
       </div>
